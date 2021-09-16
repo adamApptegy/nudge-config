54b5f3b2bfbb784ba6994a9883d9e7b5a8617630
Adam Crider <adam.crider@apptegy.com>
2021-09-16 10:05:51 -0500

    Updating scripting to pull latest JSON file from github and check for conditions before running.
    There is a check for checking if the current user is sudo.  Typically if
    this is deployed through an MDM the user will be running as sudo, but
    for testing the user needs to be elevated.
    
    Script first checks if Nudge is installed on the machine, then checks if
    Nudge is already running.  Want to avoid multiple instances of Nudge
    running.
    
    Curl the latest JSON from github and then swap the temporary file into
    the correct location.

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Check if Nudge exists before we try anything
 if [ ! -f /Applications/Utilities/Nudge.app/Contents/MacOS/Nudge ]; then
@@ -6,15 +6,34 @@ if [ ! -f /Applications/Utilities/Nudge.app/Contents/MacOS/Nudge ]; then
     exit 1
 fi
 
-# Assume Nudge exists, continue scripting.
+# Check if Nudge is currently running
+if pgrep -x "Nudge" > /dev/null
+then
+    echo "Nudge is currently running already"
+    exit 1
+fi
 
+# Check if we are a sudo user
+if [ $(echo "$UID") = "0" ]; then
+    SUDO=''
+else
+    SUDO='sudo -E'
+fi
+
+# Assume Nudge exists, continue.
+
+BASE_URL="https://raw.githubusercontent.com/adamApptegy/nudge-config/main/com.github.macadmins.Nudge.json"
+TMP_FILE="/tmp/com.github.macadmins.Nudge.json"
+TARGET_FILE="/Library/Preferences/com.github.macadmins.Nudge.json"
 #Download the latest JSON
-#curl something tmp
+/usr/bin/curl ${BASE_URL} -o ${TMP_FILE}
+
 
 #Replace the local JSON with the newly downloaded one.
-#cp old .. new
+$SUDO /bin/cp ${TMP_FILE} ${TARGET_FILE}
 
 #Remove the temporary file
-#rm tmp
+/bin/rm ${TMP_FILE}
 
 #Launch Nudge
+/Applications/Utilities/Nudge.app/Contents/MacOS/Nudge
